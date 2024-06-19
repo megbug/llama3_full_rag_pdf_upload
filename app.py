f183897978807b35bdc8c1243b2a8ce3686c8cc7
megbug <meghub@proton.me>
2024-06-19 19:29:33 +0200

    added post routes

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
 from langchain_community.llms import Ollama
+from flask import Flask, request
 
-llm = Ollama(model="llama3")
 
-response = llm.invoke("Tell me a badminton joke?")
+app = Flask(__name__)
 
-print(response)
+
+cached_llm = Ollama(model="llama3")
+
+@app.route("/ai", methods=["POST"])
+def aiPost():
+    print("Post /ai called")
+    json_content = request.json
+    query = json_content.get("query")
+
+    print(f"query: {query}")
+
+    response = cached_llm.invoke(query)
+    print(response)
+
+    response_answer = {"answer": response}
+    return response_answer
+
+@app.route("/pdf", methods=["POST"])
+def pdfPost():
+    file = request.files["file"]
+    file_name = file.filename
+    save_file = "pdf/" + file_name
+    file.save(save_file)
+    print(f"filename: {file_name}")
+
+    response = {"status": "Successfully Uploaded", "filename": file_name}
+    return response
+
+def start_app():
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
+if __name__ == "__main__":
+    start_app();
